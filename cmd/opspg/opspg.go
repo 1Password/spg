94d52328c1ffa5aa586e47a11c740c09a8fdbc3c
Jeffrey Goldberg <jeff@agilebits.com>
2018-08-23 17:08:45 -0500

    wordlists in CLI

--- cmd/opspg/opspg.go
+++ cmd/opspg/opspg.go
@@ -13,8 +13,14 @@ const (
 	rtWordlist
 )
 
+// This really should use subcommands, as the flags for char and wl will
+// have different meanings and defaults. But I haven't read up on how to do that.
 var flagRecipeType = flag.String("type", "char", "character (\"char\") or wordlist (\"wl\") recipe")
 var flagLength = flag.Int("length", 16, "Length")
+var flagWLFile = flag.String("listfile", "", "Wordlist file")
+var flagList = flag.String("list", "words", "Agilewords (\"words\") or AgileSyllables (\"syl\")")
+var flagNumber = flag.Int("n", 1, "number of passwords to generate")
+var flagEntropy = flag.Bool("e", false, "Display entropy")
 
 func main() {
 	flag.Parse()
@@ -26,18 +32,23 @@ func main() {
 	default:
 		recipeType = rtChar
 	}
+
+	f := doWordlistPassword
 	switch recipeType {
 	case rtWordlist:
-		// We don't do this yet
-		log.Fatal("word lists not yet impemented")
+		f = doWordlistPassword
 
 	case rtChar:
-		doCharacterPassword()
+		f = doCharacterPassword
 	default:
 		log.Fatalf("Unknown recipe type: %v\n", recipeType)
 	}
+	for i := 0; i < *flagNumber; i++ {
+		f()
+	}
 }
 
+// really should use subcommands
 func doCharacterPassword() {
 	r := spg.CharRecipe{
 		Length: *flagLength,
@@ -48,5 +59,48 @@ func doCharacterPassword() {
 	if err != nil {
 		log.Fatalf("Couldn't generate password: %v\n", err)
 	}
-	fmt.Println(pwd)
+	if *flagEntropy {
+		fmt.Printf("%.2f:\t%v\n", pwd.Entropy, pwd)
+	} else {
+		fmt.Println(pwd)
+	}
+}
+
+func doWordlistPassword() {
+	if len(*flagWLFile) != 0 {
+		log.Fatal("File reading not yet implemented")
+	}
+
+	var words []string
+	switch *flagList {
+	case "word":
+		words = spg.AgileWords
+	case "syl":
+		words = spg.AgileSyllables
+	default:
+		log.Fatalf("list must be either %q or %q\n", "word", "syl")
+	}
+
+	wl, err := spg.NewWordList(words)
+	if err != nil {
+		log.Fatalf("Failed initiate wordlist: %v\n", err)
+	}
+
+	// Will need more command line options for settings, but lets just
+	// hardcode stuff for now
+
+	r := spg.NewWLRecipe(*flagLength, wl)
+	r.SeparatorFunc = spg.SFDigits1
+	r.Capitalize = spg.CSOne
+
+	pwd, err := r.Generate()
+	if err != nil {
+		log.Fatalf("Couldn't generate password: %v\n", err)
+	}
+
+	if *flagEntropy {
+		fmt.Printf("%.2f:\t%v\n", pwd.Entropy, pwd)
+	} else {
+		fmt.Println(pwd)
+	}
 }
